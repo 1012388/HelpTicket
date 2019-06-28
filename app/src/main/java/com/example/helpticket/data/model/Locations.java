fad2c8aa907f030c3c6025567c98130737f768d5
Bruno Santos <36928697+1012388@users.noreply.github.com>
2019-06-28 15:09:36 +0100

    Creating the most important classes for the application. Also the classes have more comments explaining their objective

---
+++ app/src/main/java/com/example/helpticket/data/model/Locations.java
@@ -0,0 +1,42 @@
+/*Objective:This class will store the different locations of the factory, for example
+* NAV1
+* NAV2
+* NAV3
+* NAV4
+* NAV5
+* */
+
+
+package com.example.helpticket.data.model;
+
+public class Locations {
+
+    //fields
+    private int idLocations;
+    private String name;
+
+    //constructor
+
+    public Locations(int idLocations, String name) {
+        this.idLocations = idLocations;
+        this.name = name;
+    }
+
+    //gets and sets
+
+    public int getIdLocations() {
+        return idLocations;
+    }
+
+    public void setIdLocations(int idLocations) {
+        this.idLocations = idLocations;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
