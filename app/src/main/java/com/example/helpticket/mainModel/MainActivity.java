44b50069e67ad891c86e6802594f674620197303
Bruno Santos <36928697+1012388@users.noreply.github.com>
2019-07-03 17:29:31 +0100

    Creating the various layouts of the application

--- app/src/main/java/com/example/helpticket/mainModel/MainActivity.java
+++ app/src/main/java/com/example/helpticket/mainModel/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.helpticket.mainModel;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -30,4 +31,19 @@ public class MainActivity extends Activity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(requestAdapter);
     }
+
+    public void showMainActivity() {
+        //TODO:criar uma classe para mostrar o menu
+        //Intent intent = new Intent(this, showMainActivity.class);
+
+
+        // intent.putExtra();//Para passar valores de uma atividade para outra
+        // startActivity(intent);
+    }
+
+    //TODO:Criar as classes para os vários layouts
+    //TODO:Para o spinner no create fazer o que está na página https://stackoverflow.com/questions/13377361/how-to-create-a-drop-down-list
+    //TODO:Detalhes antes de for resolvido, detalhes depois de for resolvido
+    //TODO:Relatório, tem de mostrar todos os pedidos que um técnico fez nesse dia. Poder ver sempre os relatórios antigos.
+    //TODO:Create falta colocar spinner também os equipamentos que estão associados à pessoa.
 }
