3ba37c53f0657eba005cbe32e84282e3d08ec4c3
Bruno Santos <36928697+1012388@users.noreply.github.com>
2019-07-03 17:29:31 +0100

    Creating the various layouts of the application

--- app/src/main/java/com/example/helpticket/ui/login/LoginViewModel.java
+++ app/src/main/java/com/example/helpticket/ui/login/LoginViewModel.java
@@ -10,6 +10,7 @@ import com.example.helpticket.data.LoginRepository;
 import com.example.helpticket.data.Result;
 import com.example.helpticket.data.model.LoggedInUser;
 import com.example.helpticket.R;
+import com.example.helpticket.mainModel.MainActivity;
 
 public class LoginViewModel extends ViewModel {
 
@@ -36,6 +37,7 @@ public class LoginViewModel extends ViewModel {
         if (result instanceof Result.Success) {
             LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
             loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
+            //MainActivity.showMainActivity();
         } else {
             loginResult.setValue(new LoginResult(R.string.login_failed));
         }
