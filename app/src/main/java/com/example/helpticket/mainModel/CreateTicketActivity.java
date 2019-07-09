1b2905bc223c09dea94ea24160fb6a523a3928fc
Bruno Santos <36928697+1012388@users.noreply.github.com>
2019-07-09 17:21:06 +0100

    Creating an method insert new tickets into Firebase

--- app/src/main/java/com/example/helpticket/mainModel/CreateTicketActivity.java
+++ app/src/main/java/com/example/helpticket/mainModel/CreateTicketActivity.java
@@ -2,6 +2,7 @@ package com.example.helpticket.mainModel;
 
 import android.os.Bundle;
 
+import com.example.helpticket.databaseModels.Employee;
 import com.example.helpticket.databaseModels.Equipment;
 import com.example.helpticket.databaseModels.Shift_Technician;
 import com.example.helpticket.databaseModels.Ticket;
@@ -18,12 +19,16 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.helpticket.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
 import java.util.Date;
 
 public class CreateTicketActivity extends AppCompatActivity {
 
+    private DatabaseReference mDatabase;
+    private Ticket ticket;
 
 
     @Override
@@ -31,7 +36,7 @@ public class CreateTicketActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_ticket);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        //RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id);
         setSupportActionBar(toolbar);
 
         TextView textViewTech = (TextView) findViewById(R.id.textViewTech);
@@ -52,7 +57,7 @@ public class CreateTicketActivity extends AppCompatActivity {
         Spinner spinnerLoc = (Spinner) findViewById(R.id.spinnerLoc);
         final Button btnCreateTicket = (Button) findViewById(R.id.btnCreateTicket);
 
-        Date currentTime = Calendar.getInstance().getTime();
+        final Date currentTime = Calendar.getInstance().getTime();
 
         textViewDate1.setText(currentTime.toString());
         //textViewEmp1.setText();
@@ -61,12 +66,11 @@ public class CreateTicketActivity extends AppCompatActivity {
         btnCreateTicket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*idTicket++;
 
-                ticket.setIdTicket(idTicket);
-                ticket.setIdEmployee(textViewEmp1.getText().toString());
-                ticket.setDescription(textViewDesc1.getText().toString());
-*/
+                //idEquip = ;//Buscar das caixas de texto
+               // idEmp=;//Buscar das caixas de texto
+                //CreateTicket(currentTime, "Troca de HDD para SSD", false,,);
+
 
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
@@ -76,8 +80,44 @@ public class CreateTicketActivity extends AppCompatActivity {
 
     }
 
-    public void CreateTicket(){
-       // textViewTech1.s
-    }
+    public void CreateTicket(Date requested_date, String description, Boolean state, Equipment idEquipment, Employee idEmployee) {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        ticket = new Ticket(1, idEquipment, requested_date, idEmployee, description, state);
 
+        //mDatabase.child("users").child(userId).setValue(useR);
+
+
+        // [START write_message]
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        // [END write_message]
+
+    }
+/*
+    public void basicReadWrite() {
+
+
+        // [START read_message]
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+// [END read_message]
+
+    }*/
 }
