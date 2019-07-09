57610995b4603f8e235451563a3aedd2104f4ba3
Bruno Santos <36928697+1012388@users.noreply.github.com>
2019-07-09 09:22:56 +0100

    updating shift_technician

--- app/src/main/java/com/example/helpticket/databaseModels/Shift_Technician.java
+++ app/src/main/java/com/example/helpticket/databaseModels/Shift_Technician.java
@@ -6,17 +6,18 @@ public class Shift_Technician {
     private int idShift_Technician;
     private Shift idShift;
     private Technician idTechnician;
-    private Date startShift;//hora_inicio do turno
-    private Date endShift;//hora_fim do turno
 
     //construtor
+
+    public Shift_Technician(){
+
+    }
 
     public Shift_Technician(int idShift_Technician, Shift idShift, Technician idTechnician, Date startShift, Date endShift) {
         this.idShift_Technician = idShift_Technician;
         this.idShift = idShift;
         this.idTechnician = idTechnician;
-        this.startShift = startShift;
-        this.endShift = endShift;
+
     }
 
 
@@ -47,19 +48,5 @@ public class Shift_Technician {
         this.idTechnician = idTechnician;
     }
 
-    public Date getStartShift() {
-        return startShift;
-    }
 
-    public void setStartShift(Date startShift) {
-        this.startShift = startShift;
-    }
-
-    public Date getEndShift() {
-        return endShift;
-    }
-
-    public void setEndShift(Date endShift) {
-        this.endShift = endShift;
-    }
 }
