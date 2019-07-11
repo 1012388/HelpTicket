a8d722c5d9ebb180763fd0569790bccd15a98fe2
Bruno Santos <36928697+1012388@users.noreply.github.com>
2019-07-11 17:04:18 +0100

    Adding the possiblity to shearch for solved and unsolved tickets

--- app/src/main/java/com/example/helpticket/mainModel/AllTicketActivity.java
+++ app/src/main/java/com/example/helpticket/mainModel/AllTicketActivity.java
@@ -1,9 +1,16 @@
 package com.example.helpticket.mainModel;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
 
 import com.example.helpticket.R;
 
@@ -13,15 +20,44 @@ public class AllTicketActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_ticket);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button buttonSolved     = (Button) findViewById(R.id.buttonSolved);
+        Button buttonNotSolved  = (Button) findViewById(R.id.buttonNotSolved);
+
+
     }
 
     public void showUnsolvedTickets(View view) {
+        Intent intent = new Intent(this, UnsolvedTicketActivity.class);
 
+
+        startActivity(intent);
         //Cena do intent
     }
 
     public void showSolvedTickets(View view) {
 
-        //Cena do intent para a view dos
+        Intent intent = new Intent(this, SolvedTicketActivity.class);
+
+
+        startActivity(intent);
     }
+
 }
+
+/*
+public class AllTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_ticket);
+
+
+
+    }
+
+
+}*/
